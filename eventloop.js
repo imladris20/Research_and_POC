e62b7d311ef52bd36450d57cbb2d38753bd9caf4
Po Lien Lin <polien.joe.lin@gmail.com>
2024-01-08 09:00:08 +0800

    Add event loop testing

---
+++ eventloop.js
@@ -0,0 +1,17 @@
+console.log("hi");
+
+setTimeout(() => {
+  console.log("Mina");
+}, 100);
+
+setTimeout(() => {
+  console.log("Sana");
+}, 0);
+
+console.log("hello");
+
+setTimeout(() => {
+  console.log("Momo");
+}, 0);
+
+console.log("heyyyy");
