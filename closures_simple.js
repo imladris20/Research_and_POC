442e0abe3245dbcb7df2749b028f8a97a37526ae
Po Lien Lin <polien.joe.lin@gmail.com>
2024-02-05 11:40:20 +0800

    Add simple closures

---
+++ closures_simple.js
@@ -0,0 +1,24 @@
+function init() {
+  let message = "JSClosures";
+
+  function fireMessage() {
+    console.log(message);
+  }
+  return message;
+}
+
+const print = init();
+
+print();
+
+function makeAdder(x) {
+  return function (y) {
+    return x + y;
+  };
+}
+
+var add5 = makeAdder(5);
+var add10 = makeAdder(10);
+
+console.log(add5(2)); // 7
+console.log(add10(2)); // 12
