472baaafe45d96b0762dafd450bc8f2a968946c8
Po Lien Lin <polien.joe.lin@gmail.com>
2024-02-08 16:20:15 +0800

    Show effect of async await

---
+++ asyncAwait.js
@@ -0,0 +1,17 @@
+async function yoyo() {
+  // ~ 開頭表示直接執行這個 function，結尾有 ()
+  const delay = (s) => {
+    return new Promise(function (resolve) {
+      // 回傳一個 promise
+      setTimeout(resolve, s); // 等待多少秒之後 resolve()
+    });
+  };
+
+  console.log(1); // 顯示 1
+  await delay(1000); // 延遲ㄧ秒
+  console.log(2); // 顯示 2
+  await delay(2000); // 延遲二秒
+  console.log(3); // 顯示 3
+}
+
+yoyo();
