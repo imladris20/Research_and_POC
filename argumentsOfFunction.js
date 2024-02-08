d90b7d029aeb115f02737b56052678b4356e9fc8
Po Lien Lin <polien.joe.lin@gmail.com>
2024-02-08 16:19:48 +0800

    Compare arguments and rest parameters

--- argumentsOfFunction.js
+++ argumentsOfFunction.js
@@ -1,6 +1,7 @@
 const arguments = [1, 2, 3];
 
-//* 一般函式的 arguments 就會是傳進去的參數依序組成的陣列
+//* 一般函式的 arguments 就會是傳進去的參數依序組成的類陣列
+//! 請注意，arguments並不是真的陣列，是一個 array-like object
 function foo(x, y, z = 9) {
   console.log(arguments[2]);
   console.log(arguments);
@@ -12,6 +13,19 @@ function foo(x, y, z = 9) {
   return f();
 }
 console.log(foo(3, 6));
+
+//* Array methods can be used on rest parameters, but not on the arguments object
+//! 由於不是真的陣列，所以不能使用 array methods，除非先把 arguments array 化
+function fn(a, b) {
+  const normalArray = Array.prototype.slice.call(arguments);
+  // — or —
+  const normalArray2 = [].slice.call(arguments);
+  // — or —
+  const normalArrayFrom = Array.from(arguments);
+
+  const first = normalArray.shift(); // OK, gives the first argument
+  const firstBad = arguments.shift(); // ERROR (arguments is not a normal array)
+}
 
 //* 箭頭函式的arguments 則不存在，即使確實有參數也不會列出來
 const bar = (a, b) => {
--- restParameters.js
+++ restParameters.js
@@ -7,3 +7,22 @@ function multiply(multiplier, ...theArgs) {
 
 const arr = multiply(2, 15, 25, 42);
 console.log(arr); // [30, 50, 84]
+
+//* rest parameters產生的就是一個真的陣列
+//* 所以可以使用 array 的各種 method
+//! arguments 則不行，除非先把它 array 化
+
+function sortRestArgs(...theArgs) {
+  const sortedArgs = theArgs.sort();
+  return sortedArgs;
+}
+
+console.log(sortRestArgs(5, 3, 7, 1)); // 1, 3, 5, 7
+
+function sortArguments() {
+  const sortedArgs = arguments.sort();
+  return sortedArgs; // this will never happen
+}
+
+console.log(sortArguments(5, 3, 7, 1));
+// throws a TypeError (arguments.sort is not a function)
