35af7adc5b36d1854e35845447a99f878c7a4e29
Po Lien Lin <polien.joe.lin@gmail.com>
2024-03-04 11:52:17 +0800

    Add style

--- timingOfReRenderActivatedBySetState/src/App.jsx
+++ timingOfReRenderActivatedBySetState/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [list, setList] = useState(["Mina", "Sana", "Momo"]);
+  const [list, setList] = useState(["Mina", "、", "Sana", "、", "Momo"]);
   const [number, setNumber] = useState(9);
 
   const handleResetListButtonClick = () => {
@@ -14,21 +14,44 @@ function App() {
     setNumber(num);
   };
 
+  console.log("有render");
+
   return (
     <div>
-      <h1>setNumber到一樣的數字會re-render嗎？</h1>
+      <h1>setNumber 為一樣的數字會re-render嗎？</h1>
       <h3>Number:{number}</h3>
-      <button onClick={() => handleResetNumberButtonClick(9)}>
+      <button
+        onClick={() => handleResetNumberButtonClick(9)}
+        style={{
+          backgroundColor: "skyblue",
+          color: "white",
+          marginRight: "12px",
+        }}
+      >
         ResetNumber to 9
       </button>
-      <button onClick={() => handleResetNumberButtonClick(10)}>
+      <button
+        onClick={() => handleResetNumberButtonClick(10)}
+        style={{
+          backgroundColor: "skyblue",
+          color: "white",
+        }}
+      >
         ResetNumber to 10
       </button>
-      <h1>setList 到一樣的清單資料會re-render嗎？</h1>
-      <h3>List</h3>
-      {list.map((member, index) => {
-        return <p key={index}>{member}</p>;
-      })}
+      <h1>setList 為一樣的清單資料會re-render嗎？</h1>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <h3 style={{ marginRight: "8px", color: "skyblue" }}>List:</h3>
+        {list.map((member, index) => {
+          return <h3 key={index}>{member}</h3>;
+        })}
+      </div>
       <button onClick={handleResetListButtonClick}>ResetList</button>
     </div>
   );
