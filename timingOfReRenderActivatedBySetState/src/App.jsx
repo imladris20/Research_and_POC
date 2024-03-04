01effad55985355f9062a2337668660322e7baef
Po Lien Lin <polien.joe.lin@gmail.com>
2024-03-04 12:08:28 +0800

    Adjust setting button

--- timingOfReRenderActivatedBySetState/src/App.jsx
+++ timingOfReRenderActivatedBySetState/src/App.jsx
@@ -6,7 +6,12 @@ function App() {
   const [number, setNumber] = useState(9);
 
   const handleResetListButtonClick = () => {
-    const newMembers = [...list];
+    const newMembers = ["Mina", "、", "Sana", "、", "Momo"];
+    setList(newMembers);
+  };
+
+  const handleNewListButtonClick = () => {
+    const newMembers = ["Mina", "、", "Sana", "、", "Momo", "、", "Tzuyu"];
     setList(newMembers);
   };
 
@@ -18,28 +23,7 @@ function App() {
 
   return (
     <div>
-      <h1>setNumber 為一樣的數字會re-render嗎？</h1>
-      <h3>Number:{number}</h3>
-      <button
-        onClick={() => handleResetNumberButtonClick(9)}
-        style={{
-          backgroundColor: "skyblue",
-          color: "white",
-          marginRight: "12px",
-        }}
-      >
-        ResetNumber to 9
-      </button>
-      <button
-        onClick={() => handleResetNumberButtonClick(10)}
-        style={{
-          backgroundColor: "skyblue",
-          color: "white",
-        }}
-      >
-        ResetNumber to 10
-      </button>
-      <h1>setList 為一樣的清單資料會re-render嗎？</h1>
+      <h1>setNumber 成一樣的數字會re-render嗎？</h1>
       <div
         style={{
           display: "flex",
@@ -47,12 +31,60 @@ function App() {
           justifyContent: "center",
         }}
       >
-        <h3 style={{ marginRight: "8px", color: "skyblue" }}>List:</h3>
+        <h3 style={{ marginRight: "8px", color: "#ff5fa2" }}>Number:</h3>
+        <h3>{number}</h3>
+      </div>
+      <button
+        onClick={() => handleResetNumberButtonClick(9)}
+        style={{
+          backgroundColor: "#FCC89B",
+          color: "#334155",
+          marginRight: "12px",
+        }}
+      >
+        setNumber to 9
+      </button>
+      <button
+        onClick={() => handleResetNumberButtonClick(10)}
+        style={{
+          backgroundColor: "#FCC89B",
+          color: "#334155",
+        }}
+      >
+        setNumber to 10
+      </button>
+      <h1>setList 成一樣的array內容會re-render嗎？</h1>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <h3 style={{ marginRight: "8px", color: "#ff5fa2" }}>List:</h3>
         {list.map((member, index) => {
           return <h3 key={index}>{member}</h3>;
         })}
       </div>
-      <button onClick={handleResetListButtonClick}>ResetList</button>
+      <button
+        onClick={handleResetListButtonClick}
+        style={{
+          backgroundColor: "#FCC89B",
+          color: "#334155",
+          marginRight: "12px",
+        }}
+      >
+        setList to the same
+      </button>
+      <button
+        onClick={handleNewListButtonClick}
+        style={{
+          backgroundColor: "#FCC89B",
+          color: "#334155",
+        }}
+      >
+        setList to new one
+      </button>
     </div>
   );
 }
